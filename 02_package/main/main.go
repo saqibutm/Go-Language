6118adfdf37eff1852fbc7875e192f575ac9da71
Saqib Ali <saqibutm@gmail.com>
2018-07-07 10:10:34 +0800

    Created 02_package

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/saqibutm/GolangTraining/GolangTraining/02_package/stringutil"
+)
+
+func main(){
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	//fmt.Println(winniepooh.BearName)
+}
